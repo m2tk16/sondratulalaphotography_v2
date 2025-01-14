2410e80dd080c4e70a0c8d56ba9e5420abc57d1f
Martin <m2tk16@gmail.com>
2025-01-14 17:57:58 -0600

    publishing

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About/>} />
+            
             {/*
             <Route path='/portfolio' element={<Portfolio/>} />
             <Route path='/contact' element={<Contact/>} />
